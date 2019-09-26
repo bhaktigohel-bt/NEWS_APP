1b6e158452ca901ff5c70df3632889fd17b6fc6b
sharmanuj <sharmanuj003@gmail.com>
2019-09-26 16:07:09 +0530

    changes in form input field with material-ui input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { FormHelperText ,TextField} from '@material-ui/core';
 const aol = value =>
     value && /.+@aol\.com/.test(value) ?
-    'Really? You still use AOL for your email?' : undefined;
+        'Really? You still use AOL for your email?' : undefined;
 const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined;
 
 const maxValue = max => value =>
@@ -9,30 +10,35 @@ const maxValue = max => value =>
 
 const maxValue20 = maxValue(20);
 
-const inputField = ({ input, label, type, meta: { touched, error, warning } }) => ( 
-    <>
-        <label className="sr-only">{label}</label>
-        <input className="form-control form-control-sm"
-        {...input }
-        placeholder = { label }
-        type = { type }
-        /> 
-        { touched && ((error && <span> { error } </span> ) || (warning && <span> { warning } </span> )) } 
+const inputField = ({ input, label, type,id, rows, multiline, meta: { touched, error, warning } ,  ...custom }) => (
+    <>  
+        <TextField
+            error = {touched && !!error}
+            margin="dense"
+            variant="outlined"
+            type={type}
+            label={label}
+            id = {id}
+            rows = {rows}
+            multiline = {multiline}
+            {...input}
+            {...custom}
+        />
+        {touched && ((error && <span> {error} </span>) || (warning && <span> {warning} </span>))}
     </>
 )
-const selectField = ({ input, type, label, options, optionName, optionValue, defaultValue, meta: { touched, error }, ...custom }) => ( 
-    <div>
-        <label id = { label } > { label } </label> 
-        <select native input = { <input id = { label }/>} error = { touched && !!error } {...input } {...custom } >
-            <option value = "" > </option> 
-            {options.map(d => {
-                return ( 
-                    <option key = { d[optionValue] } value = { d[optionValue] } > { d[optionName] } </option>
-                    )
-                })
-            } 
-        </select> 
-        {/* {(touched && error) ? <FormHelperText error > { error } </FormHelperText>: ''}  */}
-    </div>    
+const selectField = ({ label, options, optionName, optionValue, defaultValue, meta: { touched, error }, ...custom }) => (
+    <>
+        <label className="sr-only"> {label} </label>
+        <select className="form-control form-control-sm margin-5" placeholder={label}  >
+            {options.map((d) => {
+                return (
+                    <option key={d[optionValue]} value={d[optionValue]} selected={d[optionValue] === defaultValue ? true : false} > {d[optionName]} </option>
+                )
+            })
+            }
+        </select>
+        {(touched && error) ? <FormHelperText error > { error } </FormHelperText>: ''} 
+    </>
 )
 export { inputField, selectField };
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import logo from '../../logo.svg';
+import { Button} from '@material-ui/core';
 import { Field, reduxForm } from 'redux-form';
 import { Tabs, Tab } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from './headerActions';
 import { inputField, selectField } from '../Form';
-
+import * as dashboardActions from '../Dashboard/dashBoardActions';
+import * as querystring from 'querystring';
 class Header extends React.Component {
 
     constructor() {
@@ -14,15 +16,17 @@ class Header extends React.Component {
         this.state = {
             route: 'everything',
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     HandleRoute(path) {
-        console.log('haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', path)
-        this.props.setpath(path);
+        this.props.actions.setpath(path);
     }
 
-    handleSubmit(){
-        console.log('submitting');
+    handleSubmit = (values) => {
+        console.log(values);
+        console.log(querystring.stringify(values));
+        return false;
     }
     componentWillReceiveProps(nextProps) {
         this.setState({
@@ -32,9 +36,8 @@ class Header extends React.Component {
 
     render() {
         let { route } = this.state;
-        console.log(route);
         return (
-            <div className="App-header">
+            <div className="App-header displayBlock" >
                 {/* <img src={logo} sizes={100} className="App-logo " alt="logo" /> */}
                 <Tabs id="controlled-tab-example" activeKey={route} onSelect={k => this.HandleRoute(k)} >
                     <Tab eventKey="everything" title="EveryThing" > </Tab>
@@ -42,7 +45,7 @@ class Header extends React.Component {
                     <Tab eventKey="sources" title="Sources" >  </Tab>
                 </Tabs>
                 <div>
-                    <FieldLevelValidationForm handleSubmit = {this.handleSubmit} route={route} />
+                    <FieldLevelValidationForm onSubmit={this.handleSubmit} route={route} />
                 </div>
             </div>
         )
@@ -51,19 +54,20 @@ class Header extends React.Component {
 
 let FieldLevelValidationForm = (props) => {
     const { handleSubmit, pristine, reset, submitting, route } = props;
-    console.log(route);
     return (
-            <form onSubmit={handleSubmit}>
-                <Field name="q" type="text" component={inputField} label="related with"/>
-                <Field name="domains" type="text" component={inputField} label="website"/>
-                <Field name="pagesize" type="text" component={inputField} label="page size"/>
-                <Field name="page" type="text" component={inputField} label="page"/>
-                <div>
-                    <button type="submit" disabled={submitting}>Submit</button>
-                    <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
-                </div>
-            </form>
-    )}
+        <form onSubmit={handleSubmit} className='margin-5'  autoComplete="off">
+            <Field name="q" type="text" component={inputField} label="topic" required/>
+            <Field name="domains" type="text" component={inputField} label="domain"/>
+            <Field name="pagesize" type="number" component={inputField} label="Page size"/>
+            <Field name="page" type="number" component={inputField} label="Page"/>
+            <Field name="country" type="text" component={selectField} label="country" options ={[{'country_code' : 'ae','country_name' : 'United Arab emirates'},{'country_code' : 'ar','country_name' : 'argentina'},{'country_code' : 'at','country_name' : 'austira'},{'country_code' : 'au','country_name' : 'australia	'}]} defaultValue = 'at' optionName='country_name' optionValue = 'country_code' optionValue ='country_name'/>
+            <div className="margin-5">
+                <Button type="submit" size="small" variant = "contained" color="primary" disabled={submitting}>Submit</Button>
+                <Button type="button" size="small" variant = "contained" color="primary" disabled={pristine || submitting} onClick={reset}>Clear Values</Button>
+            </div>
+        </form>
+    )
+}
 
 FieldLevelValidationForm = (reduxForm({
     form: 'searchForm',
@@ -76,8 +80,10 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return { ...bindActionCreators({ ...actions }, dispatch) }
-
+    return {
+        actions: bindActionCreators(actions, dispatch),
+        dashBoardActions: bindActionCreators(dashboardActions, dispatch)
+    }
 }
 
 Header = connect(mapStateToProps, mapDispatchToProps)(Header);
